37f70cd4a53fd2e7fda4d512286678b1c6d20746
Vishnu Vardhan Reddy Mannuru <vishnuvardhanmannuru@gmail.com>
2026-02-11 19:35:04 +0530

    Initial job portal backend

--- job_portal/settings.py
+++ job_portal/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv('django-insecure-^$sk-(u21165zvc3jwoze7kb&pc)jxny(ddn%*0decj=89)j8!')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
